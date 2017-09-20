5399ff2ceb66cdc41a2e99c32d95b9ed8e6ff93b
Ana Asins <asinsanna@gmail.com>
2017-09-20 09:50:11 +0200

    modifico index y experiencia

--- Modelo/experiencia.php
+++ Modelo/experiencia.php
@@ -11,6 +11,23 @@ class Experiencia extends db
   {
     parent::__construct();
   }
+
+  function experienciaCV($idusuario){
+    //Construimos la consulta
+    $sql="SELECT * from experienciaprofesional WHERE id_personal='".$idusuario."'";
+    //Realizamos la consulta
+    $resultado=$this->realizarConsulta($sql);
+    if($resultado!=null){
+      //Montamos la tabla de resultados
+      $tabla=[];
+      while($fila=$resultado->fetch_assoc()){
+        $tabla[]=$fila;
+      }
+      return $tabla;
+    }else{
+      return null;
+    }
+  }
 }
 
  ?>
--- index.php
+++ index.php
@@ -21,9 +21,26 @@
     <title>Mi CV.</title>
   </head>
   <body>
+    <h2>Datos personales.</h2>
     <?php
-      $usuario->datosCv($_SESSION['usuario']);
-      
+      $datosusuario=$usuario->datosCv($_SESSION['usuario']);
+      foreach ($datosusuario as $usuario) {
+        echo "<img src='".$usuario["foto"]."' alt=''> <br><br>";
+        echo "<ul><li>".$usuario['nombre']."</li>";
+        echo "<li>".$usuario['apellidos']."</li>";
+        echo "<li>".$usuario['direccion']."</li>";
+        echo "<li>".$usuario['telefono']."</li>";
+        echo "<li>".$usuario['correo']."</li>";
+        echo "<li>".$usuario['redes_sociales']."</li></ul>";
+      }
+
+      ?>
+      <h2>Experiencia laboral</h2>
+      <?php
+      $experiencialaboral=$experiencia->experienciaCV($_SESSION['usuario']);
+      foreach ($experiencialaboral as $experiencia) {
+        echo "<ul><li><b>".$experiencia['anyoinicio']."-".$experiencia['anyofinal'].": </b>" .$experiencia['empresa'].": ".$experiencia['texto']."</li></ul>";
+      }
      ?>
   </body>
 </html>
